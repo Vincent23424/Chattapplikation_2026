b2d9544781ce0a9a427afccf22c520347a09f68a
Vincent <Vincentplym07@gmail.com>
2026-04-14 11:35:32 +0200

    Initial structure: Added project references and message models

---
+++ Models/ChatMessages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChattModels
+{
+    // Grundklassen - Arv startar här
+    public abstract class MessageBase
+    {
+        public string Sender { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+
+    // En underklass för vanliga meddelanden
+    public class TextMessage : MessageBase
+    {
+        public string Content { get; set; }
+    }
+
+    // En underklass för systemhändelser (t.ex. "User joined")
+    public class SystemMessage : MessageBase
+    {
+        public string Action { get; set; }
+    }
+}
